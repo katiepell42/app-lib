5e418cb7f0c843407487d58d6abbf87e610fd28e
Katie Pell <pellk@.sonoma.edu>
2022-06-09 17:11:46 +0200

    fixed speeding up bug when you click start multiple times

--- scripts/stop2.js
+++ scripts/stop2.js
@@ -8,33 +8,35 @@ var seconds = 0;
 var minutes = 0;
 var hours = 0;
 
+var stopwatchFlag = false; // Flag to determine current state of stopwatch
+
 function startStopwatch() {
     var timeStart = new Date; // Original start time
-    setInterval(function() {
-        seconds = parseInt(seconds);
-        // minutes = parseInt(minutes);
-        // hours = parseInt(hours);
-        var timeNow = new Date; // Current time
-        let difference = (timeNow - timeStart)/1000; // Gives the difference --> seconds is diff / 100, ms is (diff % 100)/100
-        seconds = difference % 60;
-        minutes = Math.floor(difference / 60);
-        if(seconds > 10) {
-            difference = difference / 10;
-        }
-        // let secondS = '0' + seconds.toFixed(1);
-        let secDisplay = seconds.toFixed(1);
-        let minDisplay = minutes;
-        let hrDisplay = hours;
-        if(seconds < 10) {
-            secDisplay = '0' + seconds.toFixed(1);
-        }
-        if(minutes < 10) {
-            minDisplay = '0' + minutes;
-        }
-        if(minutes < 10) {
-            hrDisplay = '0' + hours;
-        }        
-
-        document.getElementById('stopwatch-display').innerHTML = hrDisplay + ':' + minDisplay + ':' + secDisplay;
-    }, 100);
+    
+    if(!stopwatchFlag) {
+        stopwatchFlag = true; // Mark the stopwatch as being "currently running"
+        setInterval(function() {
+            seconds = parseInt(seconds);
+            minutes = parseInt(minutes);
+            hours = parseInt(hours);
+            var timeNow = new Date; // Current time
+            let difference = (timeNow - timeStart)/1000; // Gives the difference --> seconds is diff / 100, ms is (diff % 100)/100
+            seconds = (difference % 60).toFixed(1); // Keep it with one decimal places
+            minutes = Math.floor(difference / 60);
+            if(seconds > 10) {
+                difference = difference / 10;
+            }
+            if(seconds < 10) {
+                seconds = '0' + seconds;
+            }
+            if(minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            if(minutes < 10) {
+                hours = '0' + hours;
+            }        
+    
+            document.getElementById('stopwatch-display').innerHTML = hours + ':' + minutes + ':' + seconds;
+        }, 100);
+    }
 }
